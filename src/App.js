2adf4b91fadde6cff72f8d699d8288a11c609270
Craig Hammond <craigham94@gmail.com>
2017-10-31 17:00:26 -0400

    Reasoned out Menu responsiveness

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { Container } from 'semantic-ui-react';
 import { Menu } from 'semantic-ui-react';
 import { Header } from 'semantic-ui-react';
 import { Image } from 'semantic-ui-react';
+import { Responsive } from 'semantic-ui-react';
+import { Dropdown } from 'semantic-ui-react';
 
 import headerImage from './images/gobbler_start_2016_crop3.jpg'
 
@@ -20,6 +22,7 @@ const items = [
 ]
 
 class App extends Component {
+
   render() {
 
     return (
@@ -30,26 +33,42 @@ class App extends Component {
           <div
             style={{
               padding: '10px',
-              backgroundColor: '#ee804b'
+              backgroundColor: '#cf5300'
             }}
           >
+            <Menu secondary>
+              <Responsive as={Menu.Item} header minWidth={1200}>
+                <Header as='h1' inverted>
+                  Great Gobbler 5k
+                </Header>
+              </Responsive>
+              <Responsive as={Menu.Item} minWidth={1200}>
+                <Menu.Menu
+                  position='right'
+                  content={
+                    <Menu
+                      style={{backgroundColor: 'inherit'}}
+                      inverted
+                      items={items}
+                    />
+                  }
+                />
+              </Responsive>
+              <Responsive as={Menu.Item} position='left' maxWidth={1200} >
+                <Dropdown text='Menu'>
+                  <Dropdown.Menu>
+                    {
+                      items.map(item => {return <Dropdown.Item key={item.key} text={item.name} />})
+                    }
+                  </Dropdown.Menu>
+                </Dropdown>
+              </Responsive>
+            </Menu>
             <Container text textAlign='center' >
-              <Header as='h1' inverted>
-                Great Gobbler 5k
-              </Header>
-              <Header as='h3' inverted>
+              <Header as='h2' inverted>
                 Thursday November 23, 2017 @ Nashua High School South. Great Gobbler 5k @ 8:00 A.M., Little Gobbler (400m) @ 7:30 A.M.
               </Header>
             </Container>
-              <Container>
-                <Menu
-                  stackable
-                  secondary
-                  inverted
-                  items={items}
-                  widths={items.length}
-                />
-              </Container>
           </div>
           <Image src={headerImage} fluid />
           <Container text textAlign='center' style={{padding: '10px'}}>
