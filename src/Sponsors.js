0f7214907ca51221dcaa39e6ff7860ee257e3edc
Craig Hammond <craig@catylist.com>
2019-07-28 17:17:05 -0400

    update sponsor info with new tier, revert url for form to original as the file seems to have been updated in place

--- src/Sponsors.js
+++ src/Sponsors.js
@@ -5,8 +5,6 @@ import {
   Container
 } from 'semantic-ui-react';
 
-import sponsorInfo from "./pdfs/Sponsorship Levels 2019 GG.pdf"
-
 class Sponsors extends Component {
   render() {
     return (
@@ -18,6 +16,14 @@ class Sponsors extends Component {
         <p>Fleet Feet Sports, Haggerty's Cleaning Service, Inc.</p>
         <Header as='h3' content='Want to become a Sponsor?' />
         <List bulleted>
+          <List.Item>
+            <List.Content>
+              <Header as='h4' content='Title Sponsor ($1200)' />
+              <List.Description>
+                Two free race numbers, space for a booth on race day, and your company name will be announced on race day, company name/logo on website and prime location on the race shirt, we will hang a banner provided by you at the event, and place your name on a mile marker. Registration discount for title sponsor employees. Company logo on the front of our race’s mascot.
+              </List.Description>
+            </List.Content>
+          </List.Item>
           <List.Item>
             <List.Content>
               <Header as='h4' content='Platinum ($1000)' />
@@ -56,7 +62,7 @@ class Sponsors extends Component {
           Your sponsorship benefits both the Nashua High School North and South Boys’ and Girls’ Cross-Country programs. The cross-country teams at North and South work to keep students active, strive for academic excellence, and promote community service.
         </p>
         <p>
-          To become a sponsor, download this form:  <a href={sponsorInfo} target="_blank" rel="noopener noreferrer">Sponsorship Form</a>
+          To become a sponsor, download this form:  <a href="http://users.neo.myregisteredsite.com/7/4/4/11194447/assets/Great_Gobbler_-_Sponsorship_Levels.pdf" target="_blank" rel="noopener noreferrer">Sponsorship Form</a>
         </p>
       </Container>
     )
