2dc70a26d8d8ae0f1f64418434e1272c285d906c
Craig Hammond <craig@craighammond.me>
2020-07-22 09:53:48 -0400

    Reduce long code lines in sponsors page

--- src/Sponsors.js
+++ src/Sponsors.js
@@ -27,7 +27,10 @@ class Sponsors extends Component {
             <List.Content>
               <Header as='h4' content='Title Sponsor ($1200)' />
               <List.Description>
-                Two free race numbers, space for a booth on race day, and your company name will be announced on race day, company name/logo on website and prime location on the race shirt, we will hang a banner provided by you at the event, and place your name on a mile marker. Registration discount for title sponsor employees. Company logo on the front of our race’s mascot.
+                Two free race numbers, space for a booth on race day, and your company name will be announced on
+                race day, company name/logo on website and prime location on the race shirt, we will hang a banner
+                provided by you at the event, and place your name on a mile marker. Registration discount for title
+                sponsor employees. Company logo on the front of our race’s mascot.
               </List.Description>
             </List.Content>
           </List.Item>
@@ -35,7 +38,9 @@ class Sponsors extends Component {
             <List.Content>
               <Header as='h4' content='Platinum ($1000)' />
               <List.Description>
-                Two free race numbers, space for a booth on race day, and your company name will be announced on race day, company name/logo on website and prime location on the race shirt, we will hang a banner provided by you at the event, and place your name on a mile marker.
+                Two free race numbers, space for a booth on race day, and your company name will be announced on
+                race day, company name/logo on website and prime location on the race shirt, we will hang a banner
+                provided by you at the event, and place your name on a mile marker.
               </List.Description>
             </List.Content>
           </List.Item>
@@ -43,7 +48,9 @@ class Sponsors extends Component {
             <List.Content>
               <Header as='h4' content='Gold ($500)' />
               <List.Description>
-                Two free race numbers, space for a booth on race day, and your company name will be announced on race day, company name/logo on website and shirt, and we will hang a banner provided by you at the event.
+                Two free race numbers, space for a booth on race day, and your company name will be announced on
+                race day, company name/logo on website and shirt, and we will hang a banner provided by you at the
+                event.
               </List.Description>
             </List.Content>
           </List.Item>
@@ -51,7 +58,8 @@ class Sponsors extends Component {
             <List.Content>
               <Header as='h4' content='Silver ($250)' />
               <List.Description>
-                One free race number, name on back of shirt, space for a booth on race day, and your company name will be announced on race day.
+                One free race number, name on back of shirt, space for a booth on race day, and your company name will
+                be announced on race day.
               </List.Description>
             </List.Content>
           </List.Item>
@@ -66,10 +74,17 @@ class Sponsors extends Component {
         </List>
         <Header as='h3' content='Why Sponsor?' />
         <p>
-          Your sponsorship benefits both the Nashua High School North and South Boys’ and Girls’ Cross-Country programs. The cross-country teams at North and South work to keep students active, strive for academic excellence, and promote community service.
+          Your sponsorship benefits both the Nashua High School North and South Boys’ and Girls’ Cross-Country
+          programs. The cross-country teams at North and South work to keep students active, strive for academic
+          excellence, and promote community service.
         </p>
         <p>
-          To become a sponsor, download this form:  <a href="http://users.neo.myregisteredsite.com/7/4/4/11194447/assets/Great_Gobbler_-_Sponsorship_Levels.pdf" target="_blank" rel="noopener noreferrer">Sponsorship Form</a>
+          To become a sponsor, download this form:
+          <a
+            href="http://users.neo.myregisteredsite.com/7/4/4/11194447/assets/Great_Gobbler_-_Sponsorship_Levels.pdf"
+            target="_blank"
+            rel="noopener noreferrer"
+          > Sponsorship Form </a>
         </p>
       </Container>
     )
