6401a7687e68c03b29eaa349a975f4557b6357e4
Craig Hammond <craig@craighammond.me>
2020-11-22 11:08:47 -0500

    Fix meta field revision not getting set.

--- src/components/seo.js
+++ src/components/seo.js
@@ -5,6 +5,8 @@ import { useStaticQuery, graphql } from "gatsby";
 
 import favicon16 from "../images/favicon.ico";
 
+const REVISION = process.env.GATSBY_GIT_SHA;
+
 function SEO({ description, lang, title, meta }) {
   const { site } = useStaticQuery(
     graphql`
@@ -24,34 +26,35 @@ function SEO({ description, lang, title, meta }) {
 
   return (
     <Helmet
-    htmlAttributes={{ lang }}
-    title={title}
-    titleTemplate={defaultTitle ? `%s | ${defaultTitle}` : null}
-		link={[
-			{ rel: "shortcut icon", sizes: "16x16", href: `${favicon16}` }
-    ]}
-    meta={[
-      {
-        name: "description",
-        content: metaDescription
-      },
-      {
-        property: "og:title",
-        content: title
-      },
-      {
-        property: "og:description",
-        content: metaDescription
-      },
-      {
-        property: "og:type",
-        content: "website"
-      },
-      {
-        name: "revision",
-        content: "%GATSBY_GIT_SHA%"
-      }
-    ].concat(meta)} />
+      htmlAttributes={{ lang }}
+      title={title}
+      titleTemplate={defaultTitle ? `%s | ${defaultTitle}` : null}
+      link={[
+        { rel: "shortcut icon", sizes: "16x16", href: `${favicon16}` }
+      ]}
+      meta={[
+        {
+          name: "description",
+          content: metaDescription
+        },
+        {
+          property: "og:title",
+          content: title
+        },
+        {
+          property: "og:description",
+          content: metaDescription
+        },
+        {
+          property: "og:type",
+          content: "website"
+        },
+        {
+          name: "revision",
+          content: REVISION
+        }
+      ].concat(meta)}
+    />
   )
 }
 
