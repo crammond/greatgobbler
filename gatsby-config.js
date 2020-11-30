f27e9c8b4dc6371614b7789a058c8b645bf6ebfa
Craig Hammond <craig@craighammond.me>
2020-11-29 23:00:37 -0500

    attempt to remove old service worker from react CRA version of website, which seems to be preventing visitors who have previously accessed the site from being able to load new versions of it.

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,6 +4,12 @@ module.exports = {
 		description: "A Nashua Community Thanksgiving Tradition. Join runners, joggers and walkers "/*for a morning of fun */+"in support of the Nashua High School North and South boys and girls cross country programs."
 	},
 	plugins: [
-		"gatsby-plugin-react-helmet"
+		"gatsby-plugin-react-helmet",
+		{
+			resolve: "gatsby-plugin-remove-serviceworker",
+			options: {
+				filename: "service-worker.js"
+			} 
+		}
 	]
 }
