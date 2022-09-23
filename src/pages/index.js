26a5b8c0f27006775f74c886141d44c8420927fd
Craig Hammond <craig@catylist.com>
2022-09-22 21:17:56 -0400

    Remove comment about link coming soon
    Link is already present

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,7 +11,6 @@ const Index = () => (
 				</p>
 				<h2>A Nashua Community Thanksgiving Tradition</h2>
 				<p>Warm up your Thanksgiving appetite at Nashua High School South at the 20th Annual Great Gobbler 5K! The Great Gobbler 5K brings together family, friends, Nashua High School cross country alumni as well as the Greater Nashua area community to start the day in a healthy way.  Join runners, joggers and walkers for a morning of fun in support of the Nashua North and South Boys and Girls Cross Country Programs.</p>
-				<p>Online registration link coming soon!</p>
 			</div>
 			<a className="register-button" href='https://runsignup.com/Race/NH/Nashua/GreatGobbler5k' target='_blank' rel="noopener noreferrer">REGISTER</a>
 		</div>
