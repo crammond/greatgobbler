6dc2b0e58319de45d88b2955502b8c145f2bf83b
Craig Hammond <craig@catylist.com>
2022-11-17 09:02:33 -0500

    Update course information

--- src/pages/course-information.js
+++ src/pages/course-information.js
@@ -10,11 +10,10 @@ class CourseInformation extends Component {
         <Layout title="Course Information">
           <h2>Course Information</h2>
           <p>
-            The 5k race course begins by the South rotunda, proceeds out through the gate by the Panther dome
-            through the student parking lots and turns right onto Riverside Drive, stays on Riverside Drive
-            for approx. .5 miles, then turns right onto Stadium Drive toward Stellos Stadium, then passes by
-            the Gate House and waterfalls and the remaining two miles are through the Mine Falls trails with a
-            finish by the South Rotunda.
+            The course begins and runs down Riverside Street, then takes a right after Conway Arena toward Stellos Stadium.
+            Staying right, runners will enter into beautiful Mine Falls park and run along the river, past the South bridge,
+            under the highway, and head right over the bridge by the soccer fields. The paved trail will take you right in
+            back toward Nashua South and into the finish by the Rotunda. <a href='https://www.strava.com/routes/3026504962025148624' target='_blank' rel="noopener noreferrer">View the full course map here</a>.
           </p>
           <p>
             In the event of inclement weather/snow or other conditions, an alternate course may be used.
