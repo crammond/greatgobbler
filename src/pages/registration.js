986c117e1990c1b599d1ad3dbe1afb1f07b2ca11
ShaneGagnon <119086391+ShaneGagnon@users.noreply.github.com>
2023-07-22 17:11:00 -0400

    2023 updates

--- src/pages/registration.js
+++ src/pages/registration.js
@@ -9,19 +9,27 @@ class Registration extends Component {
         <h2>Registration Details</h2>
         <h3>Details</h3>
         <p>
-          Registration will open August 2023!
-          Online registration closes on Monday, November 20th 2023 at 11:59 P.M. If there are still spots available, in-person registration will take place on Wednesday, November 22nd in the Nashua South cafeteria from 10:00am to 5:00pm AND race day morning at Nashua South until 7:30am.
+          Registration is now open! <a href='https://greatgobbler.enmotive.com/events/2023-great-gobbler-5k' target='_blank' rel="noopener noreferrer">here</a>.
+          Online registration closes when we reach capacity. If there are still spots available, in-person registration will take place on Wednesday, November 22nd in the Nashua South cafeteria from 10:00am to 5:00pm AND race day morning at Nashua South until 7:30am.
         </p>
         <h3>Cost</h3>
         <p>
+        EARLY BIRD PRICING NOW AVAILBLE THROUGH AUGUST 31ST
+          <br />
           $25.00 Adults (18 and older)
           <br />
           $20.00 Youth (17 and younger)
           <br />
+          Regular pricing September 1st - November 22nd
+          <br />
+          $30.00 Adults (18 and older)
+          <br />
+          $25.00 Youth (17 and younger)
+          <br />
           Little Gobbler (400m) Fun Run is FREE (ages 2-10)
         </p>
         <p>
-          Registration on race day will cost $30.00 for Adults and $25.00 for Youth
+          Registration on race day will cost $35.00 for Adults and $30.00 for Youth
         </p>
       </Layout>
     )
