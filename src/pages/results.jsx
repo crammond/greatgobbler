981f9c439a9aa780f6a4d97ef01c1520a2286490
Craig Hammond <craig@craighammond.me>
2023-11-29 14:41:00 -0500

    Add 2023 results

--- src/pages/results.jsx
+++ src/pages/results.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Layout from '../components/layout';
 
 const gobblers = [
+  {year: 2023, url: 'https://raceday.enmotive.com/#/events/2023-great-gobbler-5k/results'},
   {year: 2022, url: 'https://www.secondwindtiming.com/result-page/?id=227072'},
   {year: 2021, url: 'https://www.secondwindtiming.com/result-page/?id=187241'},
   {year: 2020, url: 'https://runsignup.com/Race/Results/99251#resultSetId-215368;perpage:100'},
