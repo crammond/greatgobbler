fec9481eb07161a96e609d73145e7d1207c74215
Craig Hammond <craig@craighammond.me>
2024-11-23 20:32:36 -0500

    Update volunteer info 2024

--- src/pages/volunteer.jsx
+++ src/pages/volunteer.jsx
@@ -6,7 +6,7 @@ const Volunteer = () => (
   <Layout title="Volunteer">
     <h2>Volunteer</h2>
     <p>
-      We always need volunteers! A sign up page will open in August 2024.
+      We always need volunteers! If you would like to volunteer, please sign up <a href="https://www.signupgenius.com/go/5080C4FACA923A3FD0-15annual#/" target="_blank" rel="noopener noreferrer">HERE</a>. There are many opportunities to help out!
     </p>
   </Layout>
 )
