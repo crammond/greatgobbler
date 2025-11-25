e9381ae523963dbfba52b6873fd705fcd8e292c3
Craig Hammond <craig@craighammond.me>
2025-11-25 11:20:55 -0500

    Update sponsors for 2025

--- src/pages/sponsors.jsx
+++ src/pages/sponsors.jsx
@@ -5,19 +5,30 @@ import Layout from '../components/layout';
 import stJospeh from '../images/st-joseph-hospital-logo.svg';
 import fleetFeet from '../images/FF_Logo_Nashua_Color.png';
 import conway from '../images/conway logo.png';
-import chewies from '../images/Chewie Logo.jpg';
+import chewies from '../images/Chewies_Playland_LLC.png';
 import prg from '../images/PRG Logo.png';
-import haggertys from '../images/haggertys_cleaning_service.png';
 import joesStateFarm from '../images/state_farm_joe_roberts_agency.jpg';
 import ridof from '../images/Ridof.jpg';
 import ryder from '../images/Ryder.png';
+import gateCitySportsMedicine from '../images/GATE_CITY_SPORTS_MEDICINE.png';
+import mcCartyElectricSolutions from '../images/McCarty_Electric_Solutions.png';
+import perezWrestlingRoom from '../images/PEREZ_WRESTLING_ROOM.png';
+import centerForDentalExcellence from '../images/CENTER_FOR_DENTAL_EXCELLENCE.jpg';
+import foxPestControl from '../images/fox_pest_control.svg';
+import guessingGamesLogo from '../images/GuessingGamesLogo.png'
 
 const imageInfo = [
   {
     href: "https://stjosephhospital.com/",
     src: stJospeh,
-    pxWidth: 300,
-    alt: "St. Jospeh Hospital"
+    pxWidth: 900,
+    alt: "St. Joseph Hospital"
+  },
+  {
+    href: "https://nashuadental.com/",
+    src: centerForDentalExcellence,
+    pxWidth: 400,
+    alt: "Center For Dental Excellence"
   },
   {
     href: "https://www.chewiesplayland.com/",
@@ -28,41 +39,70 @@ const imageInfo = [
   {
     href: "https://conwaymgmt.com/",
     src: conway,
-    pxWidth: 200,
+    pxWidth: 400,
     alt: "Conway Management"
   },
   {
     href: "https://www.fleetfeet.com/s/nashua/",
     src: fleetFeet,
-    pxWidth: 300,
+    pxWidth: 400,
     alt: "Fleet Feet Nashua"
   },
   {
-    src: haggertys,
-    pxWidth: 300,
-    alt: "Haggerty's Cleaning Service"
+    href: "https://fox-pest.com/",
+    src: foxPestControl,
+    pxWidth: 400,
+    alt: "Fox Pest Control"
   },
   {
+    href: 'https://gatecitysportsmedicine.com/',
+    src: gateCitySportsMedicine,
+    pxWidth: 300,
+    alt: "Gate City Sports Management"
+  },
+  {
+    href: 'https://guessinggames.com/',
+    src: guessingGamesLogo,
+    pxWidth: 400,
+    alt: "Guessing Games",
+    style: {
+      filter: 'brightness(0) saturate(100%)'
+    }
+  },
+  {
+    href: 'https://joerobertsinsurance.com/',
     src: joesStateFarm,
-    pxWidth: 200,
+    pxWidth: 300,
     alt: "Joe Robert's Agency — State Farm"
+  },
+  {
+    href: "https://www.mccartyelectricsolutions.com/",
+    src: mcCartyElectricSolutions,
+    pxWidth: 400,
+    alt: "McCarty's Electric Solutions"
+  },
+  {
+    href: "https://www.instagram.com/perezwrestlingroom/",
+    src: perezWrestlingRoom,
+    pxWidth: 400,
+    alt: "Perez Wrestling Room"
   },
   {
     href: "https://www.prgrugs.com/",
     src: prg,
-    pxWidth: 200,
+    pxWidth: 400,
     alt: "PRG"
   },
   {
     href: "https://www.ridofjunkremoval.com/",
     src: ridof,
-    pxWidth: 200,
+    pxWidth: 400,
     alt: "RIDOF Junk Removal"
   },
   {
     href: "https://www.ryderph.com/",
     src: ryder,
-    pxWidth: 200,
+    pxWidth: 400,
     alt: "Ryder Plumping & Heating"
   }
 ]
@@ -83,7 +123,7 @@ const Sponsors = () => (
     <h3>Thank You to Our 2025 Sponsors!</h3>
     <div id="sponsor-images">
       {imageInfo.map(i => {
-        const image = <img src={i.src} key={i.alt} width={`${i.pxWidth}px`} alt={i.alt}/>;
+        const image = <img style={i.style} src={i.src} key={i.alt} width={`${i.pxWidth}px`} alt={i.alt}/>;
         return i.href ? (
           <a href={i.href} key={i.alt} target="_blank" rel="noopener noreferrer">
             {image}
